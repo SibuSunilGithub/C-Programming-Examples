ed72a0bd6857887e9bcb2e3e1746d57dedf1fc47
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-05 22:07:52 +0530

    Print A Number Enter By The User

---
+++ 02_print_integer.c
@@ -0,0 +1,10 @@
+// C program to print an integer entered by the user...
+
+# include <stdio.h>
+ int main (){
+    int num;
+    printf("Enter an integer:\n");
+    scanf("%d",&num);
+    printf("You entered:%d",num);
+   return 0;
+ }
