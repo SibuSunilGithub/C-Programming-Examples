da029e84259977adf88486e4bc9eba83684073d6
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-06 16:33:15 +0530

    Area Of The Rectangle
    commit

---
+++ 05_area_of_rectangle.c
@@ -0,0 +1,15 @@
+// C program to find area of rectangle...
+
+#include <stdio.h>
+int main()
+{
+   float h, l, a;
+
+   printf("Enter your height:\n");
+   scanf("%f", &h);
+
+   printf("Enter your length:\n");
+   scanf("%f", &l);
+
+   printf("Area of rectangle is:%f", h * l);
+}
