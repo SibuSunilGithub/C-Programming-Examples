d5a012012a56e688469e8f8d1e263fa00635f060
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-06 16:36:18 +0530

    Find ASCII Value Of Any Character

---
+++ 06_find_ascii_value.c
@@ -0,0 +1,10 @@
+// WAP to find ascii value of a character...
+
+# include <stdio.h>
+int main (){
+    char g;
+    printf("Enter a character:\n");
+    scanf("%c",&g);
+    printf("Ascii value of %c is:\n%d",g,g);
+    return 0;
+}
