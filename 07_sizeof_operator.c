38aaf3c4051b23f9ac79680358e8eb1af5c7e660
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-06 16:38:50 +0530

    Demonstrate sizeof( ) Operator

---
+++ 07_sizeof_operator.c
@@ -0,0 +1,13 @@
+// Examples of sizeof operator...
+
+# include <stdio.h>
+    int main(){
+        int i;
+        float f;
+        double d;
+        char c;
+        printf("Size of int:%zu bytes\n",sizeof(i));
+        printf("Size of int:%zu bytes\n",sizeof(f));
+        printf("Size of int:%zu bytes\n",sizeof(d));
+        printf("Size of int:%zu bytes\n",sizeof(c));
+    }
