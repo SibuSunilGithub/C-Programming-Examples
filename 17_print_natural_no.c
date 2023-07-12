7bf0d835bd4b8deb20374f10673e4884eba91610
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-12 11:41:38 +0530

    Print Natural Numbers

---
+++ 17_print_natural_no.c
@@ -0,0 +1,14 @@
+// print natural no...
+
+# include <stdio.h>
+int main(){
+    int num;
+    printf("Enter Range:\n");
+    scanf("%d",&num);
+    for (int i = 1; i <= num; i++)
+    {
+       printf("%d\n",i);
+    }
+
+   return 0;
+}
