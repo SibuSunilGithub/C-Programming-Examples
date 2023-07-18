a4fecdf62006287987d3a0fbf6b4fd1cea34e840
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-07-18 23:59:22 +0530

    Check Number Is Palindrome Or Not

---
+++ 24_palindrome.c
@@ -0,0 +1,26 @@
+// WAP To Check Number Is Palindrome Or Not...
+
+#include <stdio.h>
+int main()
+{
+    int num, digit, rev = 0, temp;
+    printf("Enter Number:\n");
+    scanf("%d", &num);
+    temp = num;
+    while (num != 0)
+    {
+        digit = num % 10;
+        num = num / 10;
+        rev = (rev * 10) + digit;
+    }
+    if (rev == temp)
+    {
+        printf("The Number Is Palindrome.");
+    }
+    else
+    {
+        printf("The Number Is Not Palindrome.");
+    }
+
+    return 0;
+}
